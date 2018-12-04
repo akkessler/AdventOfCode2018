be3f4365fb76c2ce46aa12140381dfe37a3d3977
Andy Kessler <akkessler@users.noreply.github.com>
2018-12-04 12:34:08 -0600

    Extract loop to function so can return instead of break

--- day01/part2.py
+++ day01/part2.py
@@ -1,19 +1,19 @@
+def findDuplicateFrequency(changeList):
+    freq = 0
+    seen = [freq] # Given example states the starting freq of 0 counts towards seen list
+    while True:
+        for c in changeList:
+            freq += c
+            if freq in seen:
+                return freq
+            else:
+                seen.append(freq)
+
 # Read input into memory since we may need to loop through change list multiple times.
 changeList = []
 f = open('input.txt', 'r') 
 for line in f:
     changeList.append(int(line))
-    
-freq = 0
-foundDup = False
-seen = [freq] # Given example states the starting freq of 0 counts towards seen list
-while not foundDup:
-    for c in changeList:
-        freq += c
-        if freq in seen:
-            foundDup = True
-            break
-        else:
-            seen.append(freq)
-            
-print(freq)
+                
+dupFreq = findDuplicateFrequency(changeList)
+print(dupFreq)
