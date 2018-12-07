93e7bbfb142751de4b3d0fdd74566fcf22b3c737
Andy Kessler <akkessler@users.noreply.github.com>
2018-12-06 20:25:33 -0600

    Add solution for 4.1 and 4.2

---
+++ day04/part2.py
@@ -0,0 +1,55 @@
+import numpy as np
+import re
+from datetime import datetime
+
+time_format = '[%Y-%m-%d %H:%M]'
+guard_regex = r"Guard #(\d+) begins shift"
+
+event_offset = 19
+f = open('input.txt', 'r')
+lines = f.readlines()
+f.close()
+
+events = []
+for line in lines:
+    ts = datetime.strptime(line[:event_offset].strip(), time_format)
+    events.append((ts, line[event_offset:].strip()))
+events.sort()
+
+guards = {}
+curr_guard = None
+sleep_start = None
+for ts, text in events:
+    if text == 'falls asleep':
+        sleep_start = ts
+    elif text == 'wakes up':
+        minute_start = sleep_start.minute
+        minute_end = ts.minute
+        minutes_asleep = minute_end - minute_start
+        guards[curr_guard]['minutesAsleep'] += minutes_asleep
+        guards[curr_guard]['minuteFrequency'][minute_start:minute_end] += 1
+        sleep_start = None
+    else:
+        m = re.match(guard_regex, text)
+        curr_guard = int(m[1])
+        if curr_guard not in guards:
+            guards[curr_guard] = {
+                'minutesAsleep': 0,
+                'minuteFrequency': np.zeros(60)
+            }
+
+best_guard = None
+best_minute = None
+max_freq = -1
+for key, value in guards.items():
+    freqs = value['minuteFrequency']
+    for i in range(60):
+        m = freqs[i]
+        if m > max_freq:
+            max_freq = m
+            best_minute = i
+            best_guard = key
+
+print(best_guard)
+print(best_minute)
+print(best_guard * best_minute)
